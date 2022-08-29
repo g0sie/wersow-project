752cc3e005196ed6e04afb972148c2c6ce481d73
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-08-29 19:35:41 +0200

    add test api views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(['GET'])
 def test(request):
-    return HttpResponse('hello world')
+    return Response({'message': 'team sówki'})
