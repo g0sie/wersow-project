7ee7a6a85f20a1aff512a10af62c6674c7e24ced
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-08-30 18:44:40 +0200

    configure backend for heroku

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,3 +135,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://wersow.netlify.app",
 ]
+
+# Configure Django App for Heroku
+django_heroku.settings(locals())
