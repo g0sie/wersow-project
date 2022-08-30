de6d31c22c053da0d6b4da937a4e1f9521891c8a
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-08-30 18:21:30 +0200

    add new cors allowed origin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,4 +132,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # https://pypi.org/project/django-cors-headers/
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://wersow.netlify.app",
 ]
