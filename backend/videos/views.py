efe382aba1d0f8153ca7cd0b2922891e7efa4e7a
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-08-31 18:54:07 +0200

    add POST video method

--- backend/videos/views.py
+++ backend/videos/views.py
@@ -1,13 +1,21 @@
 from django.http import JsonResponse
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 from .models import Video
 from .serializers import VideoSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def videos_list(request):
-    videos = Video.objects.all()
-    serializer = VideoSerializer(videos, many=True)
-    return Response({'videos': serializer.data})
+    if request.method == 'GET':
+        videos = Video.objects.all()
+        serializer = VideoSerializer(videos, many=True)
+        return Response({'videos': serializer.data})
+    if request.method == 'POST':
+        serializer = VideoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
