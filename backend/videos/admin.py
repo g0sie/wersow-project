ba6149b22a4b6c12f8fa2f5541bee5bf1f6ebdf4
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-08-31 17:39:34 +0200

    add Video model

--- backend/videos/admin.py
+++ backend/videos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Video
+
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'publish_date']
+    search_fields = ['title', 'publish_date']
--- backend/videos/models.py
+++ backend/videos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    thumbnail_url = models.URLField()
+    publish_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
