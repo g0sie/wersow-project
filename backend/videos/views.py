3474de1979533153008aba2e55c081eddcb4b466
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-08-31 19:09:32 +0200

    add status 404 if video does not exist

--- backend/videos/views.py
+++ backend/videos/views.py
@@ -10,10 +10,12 @@ from .serializers import VideoSerializer
 
 @api_view(['GET', 'POST'])
 def videos_list(request):
+
     if request.method == 'GET':
         videos = Video.objects.all()
         serializer = VideoSerializer(videos, many=True)
         return Response({'videos': serializer.data})
+
     if request.method == 'POST':
         serializer = VideoSerializer(data=request.data)
         if serializer.is_valid():
@@ -21,9 +23,17 @@ def videos_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def video_details(request, id: int):
-    if request.method == 'GET':
+
+    try:
         video = Video.objects.get(id=id)
+    except Video.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
         serializer = VideoSerializer(video)
         return Response(serializer.data)
+
+    if request.method == 'PUT':
+        pass
