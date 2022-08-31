eb98ebb918cd2a15dffade7917bbe0411ea98848
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-08-31 18:09:34 +0200

    add videos list GET endpoint

--- backend/videos/views.py
+++ backend/videos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import Video
+from .serializers import VideoSerializer
+
+
+@api_view(['GET'])
+def videos_list(request):
+    videos = Video.objects.all()
+    serializer = VideoSerializer(videos, many=True)
+    return Response({'videos': serializer.data})
