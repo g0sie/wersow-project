9f42cfe652673d1840f97ec4e1c1699525edccf2
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-09-02 11:16:02 +0200

    change publish_date to DateField

--- backend/videos/models.py
+++ backend/videos/models.py
@@ -5,7 +5,7 @@ class Video(models.Model):
     title = models.CharField(max_length=100)
     url = models.URLField()
     thumbnail_url = models.URLField()
-    publish_date = models.DateTimeField()
+    publish_date = models.DateField()
 
     def __str__(self):
         return self.title
