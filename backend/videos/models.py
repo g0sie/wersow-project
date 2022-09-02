ca95242f11f005b208191100a81b9f3b1b58c925
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-09-02 12:03:26 +0200

    add GetRandomVideoManager

--- backend/videos/models.py
+++ backend/videos/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.db.models.aggregates import Count
+from random import randint
+
+
+class GetRandomVideoManager(models.Manager):
+    def get_queryset(self):
+        count = self.all().count()
+        random_index = randint(0, count - 1)
+        video = self.all()[random_index]
+        return video
 
 
 class Video(models.Model):
@@ -6,6 +16,8 @@ class Video(models.Model):
     url = models.URLField()
     thumbnail_url = models.URLField()
     publish_date = models.DateField()
+
+    random = GetRandomVideoManager()
 
     def __str__(self):
         return self.title
