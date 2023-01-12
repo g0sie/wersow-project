a55636c73ba3b1aa9158d78c310a2cd5ba287a3c
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-01-12 13:38:10 +0100

    feat(auth): add register endpoint

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+@api_view(['GET', 'POST'])
+def register(request):
+
+    if request.method == 'GET':
+        pass
+
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
