aa3fbbf85fc7380633a4080a265249cb3c06532f
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-01-16 22:09:34 +0100

    style(LoginPage): style form and LoginPage

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -4,11 +4,33 @@ import pageStyles from "./Page.module.css";
 const LoginPage = () => {
   return (
     <div className={`${pageStyles.page} ${pageStyles.pageCentered}`}>
-      <form>
+      <form className={formStyles.form}>
         <h2 className={formStyles.formHeading}>Sign in to join #teamsówki</h2>
-        <input type="email" placeholder="email address" required />
-        <input type="password" placeholder="password" required />
-        <button type="submit" className="btn">
+        <div className={formStyles.inputGroup}>
+          <label className={formStyles.label} htmlFor="login-email">
+            email:
+          </label>
+          <input
+            id="login-email"
+            name="email"
+            type="email"
+            className={formStyles.input}
+            required
+          />
+        </div>
+        <div className={formStyles.inputGroup}>
+          <label className={formStyles.label} htmlFor="login-email">
+            password:
+          </label>
+          <input
+            id="login-password"
+            name="password"
+            type="password"
+            className={formStyles.input}
+            required
+          />
+        </div>
+        <button type="submit" className={`btn btnBig ${formStyles.submitBtn}`}>
           Sign in
         </button>
       </form>
