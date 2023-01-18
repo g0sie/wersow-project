4285c02bd07afc64144208fc5f5a4618d6246e7e
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-01-18 15:52:56 +0100

    feat(LoginPage): add login functionality

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,17 +1,54 @@
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
+
 import Button from "../components/Button/Button";
+
 import formStyles from "../assets/css/form.module.css";
 import pageStyles from "./Page.module.css";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [redirect, setRedirect] = useState(false);
+  const [somethingWentWrong, setSomethingWentWrong] = useState(false);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const response = await fetch(
+      "https://wersow-api.herokuapp.com/users/login",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      }
+    );
+
+    if (response.ok) setRedirect(true);
+    else setSomethingWentWrong(true);
+  };
+
+  if (redirect) {
+    return <Navigate to=".." />;
+  }
+
   return (
     <div className={`${pageStyles.page} ${pageStyles.pageCentered}`}>
-      <form className={formStyles.form}>
+      <form className={formStyles.form} onSubmit={handleSubmit}>
         <h2 className={formStyles.formHeading}>Sign in to join #teamsówki</h2>
+
+        {/* EMAIL */}
         <div className={formStyles.inputGroup}>
           <label className={formStyles.label} htmlFor="login-email">
             email:
           </label>
           <input
+            onChange={(e) => setEmail(e.target.value)}
             id="login-email"
             name="email"
             type="email"
@@ -19,11 +56,14 @@ const LoginPage = () => {
             required
           />
         </div>
+
+        {/* PASSWORD */}
         <div className={formStyles.inputGroup}>
           <label className={formStyles.label} htmlFor="login-email">
             password:
           </label>
           <input
+            onChange={(e) => setPassword(e.target.value)}
             id="login-password"
             name="password"
             type="password"
@@ -31,9 +71,15 @@ const LoginPage = () => {
             required
           />
         </div>
+
         <Button type="submit" size="big" className={[formStyles.submitBtn]}>
           Sign in
         </Button>
+        <p className={`${formStyles.errorMsg} ${formStyles.errorMsgCentered}`}>
+          &zwnj;
+          {somethingWentWrong &&
+            "Something went wrong... Try again with different data"}
+        </p>
       </form>
     </div>
   );
