f4e151b2b87bc23660e7b682a5dc068a2e0fb577
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-01-22 03:05:11 +0100

    feat(Header): make Header responsive

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { LoggedInUserContext } from "../../App";
@@ -11,6 +11,8 @@ import logoImg from "../../assets/logo.png";
 const Header = (props: { updateUser: () => void }) => {
   const loggedInUser = useContext(LoggedInUserContext);
 
+  const [isNavActive, setIsNavActive] = useState(false);
+
   const logOut = async () => {
     await fetch("https://wersow-api.herokuapp.com/users/logout", {
       method: "POST",
@@ -18,37 +20,103 @@ const Header = (props: { updateUser: () => void }) => {
       credentials: "include",
     });
     props.updateUser();
+
+    turnOffNav();
+  };
+
+  const toggleNav = () => setIsNavActive(!isNavActive);
+  const turnOffNav = () => {
+    if (isNavActive) toggleNav();
   };
 
   return (
     <header className={styles.header}>
       {/* LOGO */}
-      <Link to="/" className={styles.logo}>
+      <Link to="/" className={styles.logo} onClick={turnOffNav}>
         <img className={styles.logoImg} src={logoImg} alt="" />
         <h1 className={styles.logoText}>wersow-project</h1>
       </Link>
 
       {/* NAVIGATION */}
-      <nav></nav>
+      <nav
+        className={
+          isNavActive ? `${styles.nav} ${styles.navActive}` : styles.nav
+        }
+      >
+        {/* nav links */}
+        <ul
+          className={
+            isNavActive
+              ? `${styles.navLinks} ${styles.navLinksActive}`
+              : styles.navLinks
+          }
+        >
+          <Link className={styles.navLink} to="/" onClick={turnOffNav}>
+            <li>Home</li>
+          </Link>
+          <Link className={styles.navLink} to="/register" onClick={turnOffNav}>
+            <li>Register</li>
+          </Link>
+          <Link className={styles.navLink} to="/" onClick={turnOffNav}>
+            <li>Home</li>
+          </Link>
+        </ul>
+        {/* ( LOG IN, SIGN UP ) / ( LOG OUT ) */}
+        <div
+          className={
+            isNavActive
+              ? `${styles.authButtons} ${styles.authButtonsActive}`
+              : styles.authButtons
+          }
+        >
+          {loggedInUser === null ? (
+            <>
+              <Link to="login" onClick={turnOffNav}>
+                <Button
+                  className={[styles.resetBtn, styles.navLink]}
+                  size="small"
+                >
+                  Log in
+                </Button>
+              </Link>
 
-      {/* ( LOG IN, SIGN UP ) / ( LOG OUT ) */}
-      <nav className={styles.authButtons}>
-        {loggedInUser === null ? (
-          <>
-            <Link to="login">
-              <Button size="small">Log in</Button>
-            </Link>
-
-            <Link to="register">
-              <Button size="small">Sign up</Button>
-            </Link>
-          </>
-        ) : (
-          <Button size="small" onClick={logOut}>
-            Log out
-          </Button>
-        )}
+              <Link to="register" onClick={turnOffNav}>
+                <Button
+                  className={[styles.resetBtn, styles.navLink]}
+                  size="small"
+                >
+                  Sign up
+                </Button>
+              </Link>
+            </>
+          ) : (
+            <div>
+              <Button
+                className={[styles.resetBtn, styles.navLink]}
+                size="small"
+                onClick={logOut}
+              >
+                Log out
+              </Button>
+            </div>
+          )}
+        </div>
+        {/* dimming */}
+        <div
+          className={
+            isNavActive
+              ? `${styles.dimming} ${styles.dimmingActive}`
+              : styles.dimming
+          }
+          onClick={turnOffNav}
+        ></div>
       </nav>
+
+      <button className={styles.hamburger} onClick={toggleNav}>
+        <div className={styles.hamburgerLine}></div>
+        <div className={styles.hamburgerLine}></div>
+        <div className={styles.hamburgerLine}></div>
+      </button>
     </header>
   );
 };
