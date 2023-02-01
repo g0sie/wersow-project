0cd9639bfb0ea2c7542e88136259dcf5a895576c
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-02-01 20:46:12 +0100

    docs(users): decorate register view

--- backend/users/views.py
+++ backend/users/views.py
@@ -6,79 +6,94 @@ from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework import status
+
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from .serializers import UserSerializer
 from .models import User
 
 
-@api_view(['POST'])
+register_response = openapi.Response("User created", UserSerializer)
+
+
+@swagger_auto_schema(
+    method="POST",
+    operation_description="register a new user",
+    request_body=UserSerializer,
+    responses={201: register_response, 400: "Invalid request data"},
+)
+@api_view(["POST"])
 def register(request):
 
-    if request.method == 'POST':
+    if request.method == "POST":
         serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def login(request):
 
-    if request.method == 'POST':
-        email = request.data['email']
-        password = request.data['password']
+    if request.method == "POST":
+        email = request.data["email"]
+        password = request.data["password"]
 
         user = User.objects.filter(email=email).first()
 
         if user is None:
-            raise AuthenticationFailed('User not found')
+            raise AuthenticationFailed("User not found")
 
         if not user.check_password(password):
-            raise AuthenticationFailed('Incorrect password')
+            raise AuthenticationFailed("Incorrect password")
 
         payload = {
-            'id': user.id,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
-            'iat': datetime.datetime.utcnow()
+            "id": user.id,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            "iat": datetime.datetime.utcnow(),
         }
 
-        token = jwt.encode(payload, settings.JWT_SECRET_KEY,
-                           algorithm='HS256').decode('utf-8')
+        token = jwt.encode(payload, settings.JWT_SECRET_KEY, algorithm="HS256").decode(
+            "utf-8"
+        )
 
         response = Response()
-        response.set_cookie(key='jwt', value=token,
-                            httponly=True, samesite='None', secure=True)
-        response.data = {'jwt': token}
+        response.set_cookie(
+            key="jwt", value=token, httponly=True, samesite="None", secure=True
+        )
+        response.data = {"jwt": token}
 
         return response
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def user(request):
 
-    if request.method == 'GET':
-        token = request.COOKIES.get('jwt')
+    if request.method == "GET":
+        token = request.COOKIES.get("jwt")
 
         if not token:
-            raise AuthenticationFailed('Unauthenticated')
+            raise AuthenticationFailed("Unauthenticated")
 
         try:
-            payload = jwt.decode(
-                token, settings.JWT_SECRET_KEY, algorithm=['HS256'])
+            payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithm=["HS256"])
         except jwt.ExpiredSignatureError:
-            raise AuthenticationFailed('Unauthenticated')
+            raise AuthenticationFailed("Unauthenticated")
 
-        user = User.objects.filter(id=payload['id']).first()
+        user = User.objects.filter(id=payload["id"]).first()
         serializer = UserSerializer(user)
 
         return Response(serializer.data)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def logout(request):
 
-    if request.method == 'POST':
+    if request.method == "POST":
         response = Response()
-        response.delete_cookie('jwt', samesite='None')
-        response.data = {'message': "success"}
+        response.delete_cookie("jwt", samesite="None")
+        response.data = {"message": "success"}
         return response
