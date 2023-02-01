42ac1ce9696b3c41c5cf3f18fa109669789ebb37
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-02-01 19:36:42 +0100

    style(urls): sort paths

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,19 +1,3 @@
-"""backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from argparse import Namespace
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,9 +7,9 @@ from drf_yasg.views import get_schema_view as swagger_get_schema_view
 
 schema_view = swagger_get_schema_view(
     openapi.Info(
-        title="Wersow API",
+        title="wersow-project API",
         default_version="1.0.0",
-        description="API documentation of Wersow App",
+        description="API documentation of wersow-project",
     ),
     public=True,
     permission_classes=[DjangoModelPermissionsOrAnonReadOnly],
@@ -33,19 +17,11 @@ schema_view = swagger_get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="swagger-schema",
+    ),
     path("users/", include("users.urls")),
     path("videos/", include("videos.urls")),
-    path(
-        "",
-        include(
-            [
-                path("videos/", include(("videos.urls"), namespace="videos")),
-                path(
-                    "swagger/schema/",
-                    schema_view.with_ui("swagger", cache_timeout=0),
-                    name="swagger-schema",
-                ),
-            ]
-        ),
-    ),
 ]
