df6b696921c6d36d025a375b9b3827701f52281c
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-02-05 00:28:03 +0100

    feat(clock): call add_latest_video() at 23:55

--- backend/clock.py
+++ backend/clock.py
@@ -1,8 +1,13 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from videos.functions import change_todays_video
+from videos.functions import change_todays_video, add_latest_video
 
 sched = BlockingScheduler({"apscheduler.timezone": "Europe/Warsaw"})
+
+
+@sched.scheduled_job("cron", hour=23, minute=55)
+def scheduled_job():
+    add_latest_video()
 
 
 @sched.scheduled_job("cron", hour=0, minute=0)
