0b2cccf88c2fee2eafa31067316b848cc89a5325
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-02-08 13:18:53 +0100

    fix(clock): set only one scheduled job

--- backend/clock.py
+++ backend/clock.py
@@ -5,13 +5,9 @@ from videos.functions import change_todays_video, add_latest_video
 sched = BlockingScheduler({"apscheduler.timezone": "Europe/Warsaw"})
 
 
-@sched.scheduled_job("cron", hour=23, minute=55)
-def scheduled_job():
-    add_latest_video()
-
-
 @sched.scheduled_job("cron", hour=0, minute=0)
 def scheduled_job():
+    add_latest_video()
     change_todays_video()
 
 
