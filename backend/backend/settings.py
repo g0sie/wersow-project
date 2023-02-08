6c7aca10604f3a1ea016db6a8747cf3e43788fb4
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-02-08 14:16:50 +0100

    feat(settings): add sql logging

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -158,3 +158,27 @@ django_heroku.settings(locals())
 
 AUTH_USER_MODEL = "users.User"
 JWT_SECRET_KEY = env("JWT_SECRET_KEY")
+
+# SQL logging
+
+LOGGING = {
+    "version": 1,
+    "filters": {
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "filters": ["require_debug_true"],
+            "class": "logging.StreamHandler",
+        }
+    },
+    "loggers": {
+        "django.db.backends": {
+            "level": "DEBUG",
+            "handlers": ["console"],
+        }
+    },
+}
