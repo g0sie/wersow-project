1074d1d1dfbd58969b05bf4e97487deebc3ce663
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-02-09 22:05:54 +0100

    fix(axios): set baseURL depending on environment

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://wersow-api.herokuapp.com/"
+    : "http://127.0.0.1:8000/";
+
 const instance = axios.create({
-  baseURL: "https://wersow-api.herokuapp.com/",
+  baseURL: baseURL,
   timeout: 10000,
 });
 
