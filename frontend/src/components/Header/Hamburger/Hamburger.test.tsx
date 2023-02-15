ebf5ba8fc0a0f4a03af43e433ea1c997364800a9
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-02-15 15:07:48 +0100

    test(Hamburger): add "toggles menu on click" test

--- frontend/src/components/Header/Hamburger/Hamburger.test.tsx
+++ frontend/src/components/Header/Hamburger/Hamburger.test.tsx
@@ -1,9 +1,25 @@
 import "@testing-library/jest-dom/";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
+
+import { NavContext } from "../../../context/NavContext";
 
 import Hamburger from "./Hamburger";
 
-test("renders component", () => {
-  const { getByTestId } = render(<Hamburger />);
-  expect(getByTestId("hamburger")).toBeInTheDocument();
+describe("<Hamburger />", () => {
+  const nav = { isOpened: false, setIsOpened: jest.fn, toggle: jest.fn };
+  const { getByTestId } = render(
+    <NavContext.Provider value={nav}>
+      <Hamburger />
+    </NavContext.Provider>
+  );
+  const hamburger = getByTestId("hamburger");
+
+  it("renders component", () => {
+    expect(getByTestId("hamburger")).toBeInTheDocument();
+  });
+
+  it("toggles menu on click", () => {
+    fireEvent(hamburger, new Event("click"));
+    expect(nav.toggle).toHaveBeenCalled;
+  });
 });
