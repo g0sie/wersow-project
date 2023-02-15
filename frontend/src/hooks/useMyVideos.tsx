b7c24b6136ecbfc99e0609d566530621cb019354
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-02-16 00:29:11 +0100

    feat(MyVideos): add useMyVideos hook

---
+++ frontend/src/hooks/useMyVideos.tsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+import { LoggedInUserContext } from "../context/LoggedInUserContext";
+
+import axios from "../api";
+import { CollectedVideoInterface } from "../interfaces/CollectedVideoInterface";
+
+const useMyVideos = () => {
+  const { user } = useContext(LoggedInUserContext);
+
+  const videos = useQuery<CollectedVideoInterface[], Error>({
+    queryKey: ["myVideos"],
+    queryFn: () => {
+      return axios
+        .get(`users/${user?.id}/videos`)
+        .then((res) => res.data.videos);
+    },
+    enabled: !!user,
+  });
+
+  return videos;
+};
+
+export default useMyVideos;
--- frontend/src/interfaces/CollectedVideoInterface.ts
+++ frontend/src/interfaces/CollectedVideoInterface.ts
@@ -1,9 +1,9 @@
-export interface VideoInterface {
+export interface CollectedVideoInterface {
   id: number;
   title: string;
   url: string;
-  thumbnailUrl: string;
-  publishDate: string;
+  thumbnail_url: string;
+  publish_date: string;
   todays: boolean;
   collected: string;
 }
