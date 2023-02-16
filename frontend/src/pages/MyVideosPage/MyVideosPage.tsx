cf087e02c9dc8a7c03623be8cad7694bef653159
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-02-16 01:44:29 +0100

    feat(MyVideos): style the page

--- frontend/src/pages/MyVideosPage/MyVideosPage.tsx
+++ frontend/src/pages/MyVideosPage/MyVideosPage.tsx
@@ -1,34 +1,63 @@
 import { useContext, useEffect } from "react";
-import { LoggedInUserContext } from "../../context/LoggedInUserContext";
 import useMyVideos from "../../hooks/useMyVideos";
+
+import { LoggedInUserContext } from "../../context/LoggedInUserContext";
+
 import pageStyles from "../Page.module.css";
+import styles from "./MyVideosPage.module.css";
 
 const MyVideosPage = () => {
-  const { data: videos, isSuccess, refetch: refetchVideos } = useMyVideos();
+  const {
+    data: videos,
+    isLoading,
+    isSuccess,
+    refetch: refetchVideos,
+  } = useMyVideos();
   const { user } = useContext(LoggedInUserContext);
+
+  const message = (text: string) => {
+    return (
+      <p
+        className={[
+          styles.message,
+          pageStyles.page,
+          pageStyles.pageCentered,
+        ].join(" ")}
+      >
+        {text}
+      </p>
+    );
+  };
 
   useEffect(() => {
     refetchVideos();
-    console.log(videos);
-    videos && videos.length > 0 && console.log(videos[0].thumbnail_url);
-  }, [user, refetchVideos, videos]);
+  }, [user, refetchVideos]);
 
-  return (
-    <div
-      className={[pageStyles.page, pageStyles.pageCentered].join(" ")}
-      style={{ color: "white", fontSize: "3rem" }}
-      data-testid="videos-page"
-    >
-      {isSuccess && Array.isArray(videos)
-        ? videos.map((video) => (
+  if (isLoading) {
+    return message("Loading...");
+  }
+
+  if (isSuccess) {
+    return (
+      <div
+        className={[styles.videos, pageStyles.page].join(" ")}
+        data-testid="videos-page"
+      >
+        {videos.map((video) => (
+          <div className={styles.video} key={video.id}>
             <img
               src={video.thumbnail_url}
               alt={`thumbnail of ${video.title}`}
+              className={styles.thumbnail}
             />
-          ))
-        : "Loading..."}
-    </div>
-  );
+            <p className={styles.title}>{video.title}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return message("Failed to load videos :(");
 };
 
 export default MyVideosPage;
