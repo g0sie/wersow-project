70f6d46e1ad356ae61d257e098e6eb5ad741e4ae
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-02-17 20:35:46 +0100

    feat(Background): move it on mouse move

--- frontend/src/components/SpaceBackground/SpaceBackground.tsx
+++ frontend/src/components/SpaceBackground/SpaceBackground.tsx
@@ -1,8 +1,17 @@
+import { WithMousePositionProps } from "../../hoc/withMousePosition";
+import withMousePosition from "../../hoc/withMousePosition";
 import styles from "./SpaceBackground.module.css";
 
-const SpaceBackground = () => {
+const SpaceBackground = (props: WithMousePositionProps) => {
+  console.log(props.mouseX, props.mouseY);
+
   return (
-    <section className={styles.background}>
+    <section
+      className={styles.background}
+      style={{
+        translate: `${props.mouseX / 150}px ${props.mouseY / 100}px`,
+      }}
+    >
       <div className={styles.starsSmall}></div>
       <div className={styles.starsMedium}></div>
       <div className={styles.starsLarge}></div>
@@ -10,4 +19,4 @@ const SpaceBackground = () => {
   );
 };
 
-export default SpaceBackground;
+export default withMousePosition(SpaceBackground);
