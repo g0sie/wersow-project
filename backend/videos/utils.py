d6c57a7204c286401f5e90be990a8eb75aa6810d
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-03-13 13:27:43 +0100

    perf(videos): make get_video_urls a lazy method

--- backend/videos/utils.py
+++ backend/videos/utils.py
@@ -12,11 +12,16 @@ class WersowChannel:
         self.channel = Channel(
             "https://www.youtube.com/channel/UCtVy1X-hcxAL2ZlS6TqMQFw"
         )
+        self.video_urls = None
 
     def get_video_urls(self) -> List[str]:
         """Return list of channel's video urls."""
-        return self.channel.video_urls
+        if self.video_urls is None:
+            self.video_urls = self.channel.video_urls
+
+        return self.video_urls
 
     def get_latest_video_url(self) -> str:
         """Return url of the latest Wersow's video."""
-        return self.channel.video_urls[0]
+        urls = self.get_video_urls()
+        return urls[0]
