552aa9fd210c9e4deae5b98c135f2bdbff9e1763
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-03-14 09:25:08 +0100

    test(videos): add test for addlatestvideo command

--- backend/videos/tests/test_commands.py
+++ backend/videos/tests/test_commands.py
@@ -11,7 +11,6 @@ from videos.models import Video
 EXAMPLE_URLS = [
     "https://www.youtube.com/watch?v=cRwx3UZMjSI",
     "https://www.youtube.com/watch?v=8oYBwypAndE",
-    "https://www.youtube.com/watch?v=Obbi-NZu7IA",
 ]
 
 
@@ -31,7 +30,7 @@ class VideoCommandsTests(TestCase):
     def test_loadvideos_limit_works(self, patched_video_url_by):
         """Test that limit argument in loadvideos commands works."""
         patched_video_url_by.side_effect = EXAMPLE_URLS + [IndexError]
-        limit = 2
+        limit = 1
         call_command("loadvideos", limit=limit)
 
         added_count = Video.objects.all().count()
@@ -40,7 +39,7 @@ class VideoCommandsTests(TestCase):
     @patch("videos.utils.WersowChannel.get_video_url_by")
     def test_loadvideos_doesnt_add_duplicates(self, patched_video_url_by):
         """Test loadvideos adds only new videos."""
-        Video.objects.add_video(EXAMPLE_URLS[1])
+        Video.objects.add_video(EXAMPLE_URLS[0])
 
         patched_video_url_by.side_effect = EXAMPLE_URLS + [IndexError]
         call_command("loadvideos")
@@ -54,3 +53,10 @@ class VideoCommandsTests(TestCase):
         call_command("changetodaysvideo")
 
         change_todays_video.assert_called_once()
+
+    @patch("videos.models.Video.objects.add_latest_video")
+    def test_addlatestvideo_calls_add_latest_video(self, add_latest_video):
+        """Test addlatestvideo calls add_latest_video method."""
+        call_command("addlatestvideo")
+
+        add_latest_video.assert_called_once()
