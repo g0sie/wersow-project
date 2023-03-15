7d3f4e97f0fda42056875082a07eabe7611b24cc
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-03-15 12:11:12 +0100

    feat(videos): register Video in django-admin

--- backend/videos/admin.py
+++ backend/videos/admin.py
@@ -1,10 +1,18 @@
-# from django.contrib import admin
+"""
+Django admin for videos.
+"""
 
-# from .models import Video
+from django.contrib import admin
+
+from videos.models import Video
 
 
-# @admin.register(Video)
-# class VideoAdmin(admin.ModelAdmin):
-#     list_display = ['title', 'publish_date', 'todays']
-#     search_fields = ['title', 'publish_date']
-#     list_filter = ['todays']
+class VideoAdmin(admin.ModelAdmin):
+    """Define Video in django-admin."""
+
+    ordering = ["-todays", "-publish_date"]
+    search_fields = ["title", "publish_date"]
+    list_display = ["title", "publish_date", "todays"]
+
+
+admin.site.register(Video, VideoAdmin)
